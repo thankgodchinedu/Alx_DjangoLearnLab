439c287e2c5d3fcbaf8adbd392804f3730554b0e
Chinedu ThankGod Ugwu <ugwuthankgodchinedu1@gmail.com>
2026-01-13 06:47:56 +0100

    Fix librarian query to match checker pattern

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -16,4 +16,4 @@ def get_books_in_library(library_name):
 # Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian
+    return Librarian.objects.get(library=library)
