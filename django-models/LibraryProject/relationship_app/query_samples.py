b5d3ddf81f135985a53d317e4087331360808488
Chinedu ThankGod Ugwu <ugwuthankgodchinedu1@gmail.com>
2026-01-13 06:44:24 +0100

    Fix author query to use Book.objects.filter(author=author)

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -4,7 +4,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 # Query all books by a specific author
 def get_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    return author.books.all()
+    return Book.objects.filter(author=author)
 
 
 # List all books in a library
