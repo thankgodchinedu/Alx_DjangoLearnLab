49e2622a606b22f5acff65f841984824ed7d70a6
Chinedu ThankGod Ugwu <ugwuthankgodchinedu1@gmail.com>
2026-01-15 16:26:58 +0100

    Update auth URL patterns

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,18 +1,28 @@
 from django.urls import path
-from .views import (
-    list_books,
-    LibraryDetailView,
-    UserLoginView,
-    UserLogoutView,
-    register
-)
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+from .views import list_books, LibraryDetailView
+
+
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 
     # Authentication URLs
-    path('login/', UserLoginView.as_view(), name='login'),
-    path('logout/', UserLogoutView.as_view(), name='logout'),
-    path('register/', register, name='register'),
+    path(
+        'login/',
+        LoginView.as_view(template_name='relationship_app/login.html'),
+        name='login'
+    ),
+    path(
+        'logout/',
+        LogoutView.as_view(template_name='relationship_app/logout.html'),
+        name='logout'
+    ),
+    path(
+        'register/',
+        views.register,
+        name='register'
+    ),
 ]
