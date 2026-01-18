d039c58dd8687f7a36b7758edd7780649871a0b3
Chinedu ThankGod Ugwu <ugwuthankgodchinedu1@gmail.com>
2026-01-18 20:13:31 +0100

    fix user profiles role choices

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Author(models.Model):
@@ -27,3 +30,28 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+
+    
+class UserProfile(models.Model):
+    ADMIN = 'Admin'
+    MEMBER = 'Member'
+    LIBRARIAN = 'Librarian'
+    
+
+    ROLE_CHOICES = [
+        (ADMIN, 'Admin'),
+        (LIBRARIAN, 'Librarian'),
+        (MEMBER, 'Member'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
